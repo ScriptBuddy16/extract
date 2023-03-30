9a350a0ff3587bd7e317096480b1fc6222c7458e
ScriptBuddy16 <89794297+ScriptBuddy16@users.noreply.github.com>
2023-03-31 03:39:57 +0530

    Update config.py

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ import os
 class Config(object):
     # get a token from @BotFather
     BOT_TOKEN = os.environ.get("BOT_TOKEN", "6254464767:AAHElayeALfEM2cbWduulGOSlUj1Euz12vM")
-    API_ID = int(os.environ.get("API_ID", "29115376")
+    API_ID = int(os.environ.get("API_ID", "29115376"))
     API_HASH = os.environ.get("API_HASH", "f1f760713aa29df4983cd7aef1b035a2")
-    AUTH_USERS = "5207969772"
+    AUTH_USERS = "@5207969772"
 
